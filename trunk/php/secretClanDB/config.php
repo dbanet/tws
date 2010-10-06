f3566ed09018c071138f85d0a47f0b5332bc8137
lookias <lookias@d9403ada-bbf3-4dc9-b82b-ef610cf45845>
2010-10-06 13:36:35 +0000

    git-svn-id: http://svn.code.sf.net/p/thewheatsnooper/code@169 d9403ada-bbf3-4dc9-b82b-ef610cf45845

--- trunk/php/secretClanDB/config.php
+++ trunk/php/secretClanDB/config.php
@@ -1,10 +1,7 @@
 <?php
 	$hostname	= 'localhost';
 	$dbname		= 'lookias';
-	$username	= 'lookias';
-	//$password	= 'GQd1XVNZwOCvU3ZYqCT0';
-	$password	= '0peration';	
-	
+	$username	= 'lookias';	
 	//$db			= new PDO( 'mysql:host='.$hostname.';dbname='.$dbname, $username, $password, $options );
 	mysql_connect( $hostname, $username, $password);
 	mysql_select_db($dbname);
