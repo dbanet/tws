fe351b0ca3e0644cbda3adcfadfb5ba36b2cdbff
dbanet <admin@dbanet.org>
2014-04-30 17:30:30 +0400

    ircMessage class now works properly

--- src/wormnet/ircmessage.cpp
+++ src/wormnet/ircmessage.cpp
@@ -8,35 +8,47 @@
 ircMessage::ircMessage(QString message){
     if(message.isEmpty()) throw new QString("The message was empty.");
 
-    /* check if the message is issued by a user, e. g. ":uname!ident@uhost QUIT..." */
+    /***********************************************/
+    /* check if the message is issued by a user,   */
+    /* e. g. ":uname!ident@uhost QUIT..."          */
+    /***********************************************/
     if(message.startsWith(IRC_MSG_PREFIX_INDICATOR)){
         this->type=USER;
         this->prefix=message.split(IRC_MSG_PREFIX_SEPARATOR)[0].remove(0,QString(IRC_MSG_PREFIX_INDICATOR).length());
     }
     else this->type=SERVER;
 
-    /* check if the message contains the trailing part, e. g. "... PRIVMSG :lorem ipsum dolor sit amet" */
-    if(message.contains(" :")){
+    /***********************************************/
+    /* check if the message contains the trailing, */
+    /* e. g. "... PRIVMSG :lorem ipsum"            */
+    /***********************************************/
+    if(message.contains(IRC_MSG_TRAILING_SEPARATOR)){
         this->hasTrailPart=true;
-        this->trailing=message.section(" :",1);
+        this->trailing=message.section(IRC_MSG_TRAILING_SEPARATOR,1);
     }
     else this->hasTrailPart=false;
 
-    /* getting the command and params */
-    QStringList cmdAndParam=
-    message.mid(
-        this->type==USER?   /* if there is a prefix, exclude it   */
-            message.indexOf(IRC_MSG_PREFIX_SEPARATOR)+QString(IRC_MSG_PREFIX_SEPARATOR).length()
-          :
-            0,
-        this->hasTrailPart? /* if there is a trailing, exclude it */
-            message.indexOf(IRC_MSG_TRAILING_SEPARATOR)-1
-          :
-            -1
-    ).split(' ');                 /* the words are separated by spaces */
+    /***********************************************/
+    /*       getting the command and params        */
+    /***********************************************/
+
+    /* if there is a prefix, exclude it   */
+    int left=this->type==USER?
+    message.indexOf(IRC_MSG_PREFIX_SEPARATOR)+QString(IRC_MSG_PREFIX_SEPARATOR).length()
+    :0;
+
+    /* if there is a trailing, exclude it */
+    int right=this->hasTrailPart?
+    message.indexOf(IRC_MSG_TRAILING_SEPARATOR)-left
+    :-1;
+
+    QStringList cmdAndParam=message.mid(left,right)
+                     .split(' '); /* the words are separated by spaces */
     this->command=cmdAndParam[0]; /* the first word is the command,    */
     cmdAndParam.removeFirst();    /* and the remaining words           */
     this->paramList=cmdAndParam;  /* are the parameters of the command */
+
+    this->message=message;
 }
 bool ircMessage::isServMsg(){
     return this->type==SERVER;
@@ -46,4 +58,7 @@ bool ircMessage::isUserMsg(){
 }
 bool ircMessage::hasTrailing(){
     return this->hasTrailPart;
+}
+QString ircMessage::getRaw(){
+    return this->message;
 }
