d88e4606f17cb7a9319713522e0112c85ed0fd12
dbanet <admin@dbanet.org>
2014-05-29 21:14:41 +0300

    StepS has found a shorter TUS thread link. xD

--- src/wormnet/about.cpp
+++ src/wormnet/about.cpp
@@ -3,7 +3,7 @@
 
 #define LOOKIAS_HOMEPAGE  "<a href='http://lookias.worms2d.info'>http://lookias.worms2d.info</a>"
 #define OLD_TWS_FORUM     "<a href='http://lookias.worms2d.info/forum'>http://lookias.worms2d.info/forum/</a>"
-#define NEW_TUS_TWS_FORUM "<a href='http://www.tus-wa.com/forums/worms-armageddon/new-the-wheat-snooper-2-9-community-build-24418'>http://www.tus-wa.com/forums/worms-armageddon/new-the-wheat-snooper-2-9-community-build-24418</a>"
+#define NEW_TUS_TWS_FORUM "<a href='http://tus-wa.com/?topic=24418'>http://tus-wa.com/?topic=24418</a>"
 #define GITHUB_PAGE       "<a href='http://github.com/dbanet/tws'>http://github.com/dbanet/tws</a>"
 
 QString About::version=TWS_VERSION;
